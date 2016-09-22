2494d3a8a2c7c00c1a406001294370834aae6af6
Christian Melendez <christianhxc@gmail.com>
2016-09-21 21:49:25 -0600

    .

--- reportes/uceti/fluid.php
+++ reportes/uceti/fluid.php
@@ -1,17 +1,32 @@
 <?php
 require_once('libs/excel/PHPExcel.php');
 
+function GuardarDatos($excel, $sheet, $data, $columns, $fromRow){
+    $excel->setActiveSheetIndex($sheet);
+
+    $row = $fromRow;
+    foreach ($data as $i => $record){
+        $record = array_values($record);
+
+        foreach ($columns as $key => $column){
+            $excel->getActiveSheet()->setCellValue($column.$row, $record[$key]);
+        }
+
+        $row++;
+    }
+}
+
 $template = "PanamaFluID2016.xlsx";
 
 $excelReader = PHPExcel_IOFactory::createReader('Excel2007');
 $excelReader->setIncludeCharts(TRUE);
 $excel = $excelReader->load($template);
 
-$excel->setActiveSheetIndex(0);
-$excel->getActiveSheet()->setCellValue('C6', '4')
-    ->setCellValue('C7', '5')
-    ->setCellValue('C8', '6')
-    ->setCellValue('C9', '7');
+$data[] = array("FLU" => 4, "FLU2" => 8);
+$data[] = array("FLU" => 6, "FLU2" => 10);
+$data[] = array("FLU" => 8, "FLU2" => 12);
+$data[] = array("FLU" => 10, "FLU2" => 14);
+GuardarDatos($excel, 0, $data, array("C","D"), 6);
 
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment;filename="FluID.xlsx"');
