2b2d109532051277273bf1d8a2086a5cacb6ef60
Christian Melendez <christianhxc@gmail.com>
2016-10-08 16:59:04 -0600

    .

---
+++ db/minsa-prod/backup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE sisvigdb_bkp.cat_evento SELECT * FROM sisvigdb.cat_evento;
+CREATE TABLE sisvigdb_bkp.vih_form SELECT * FROM sisvigdb.vih_form;
+CREATE TABLE sisvigdb_bkp.vih_enfermedad_oportunista SELECT * FROM sisvigdb.vih_enfermedad_oportunista;
+CREATE TABLE sisvigdb_bkp.vih_muestra_prueba_silab SELECT * FROM sisvigdb.vih_muestra_prueba_silab;
+CREATE TABLE sisvigdb_bkp.vih_silab_temp SELECT * FROM sisvigdb.vih_silab_temp;
+CREATE TABLE sisvigdb_bkp.vih_silab_temp_factor_riesgo SELECT * FROM sisvigdb.vih_silab_temp_factor_riesgo;
+CREATE TABLE sisvigdb_bkp.vih_tarv SELECT * FROM sisvigdb.vih_tarv;
---
+++ db/minsa-prod/remove.sql
@@ -0,0 +1,8 @@
+DROP TABLE `sisvigdb`.`vih_enfermedad_oportunista`;
+DROP TABLE `sisvigdb`.`vih_factor_riesgo`;
+DROP TABLE `sisvigdb`.`vih_muestra_prueba_silab`;
+DROP TABLE `sisvigdb`.`vih_muestra_silab`;
+DROP TABLE `sisvigdb`.`vih_silab_temp`;
+DROP TABLE `sisvigdb`.`vih_silab_temp_factor_riesgo`;
+DROP TABLE `sisvigdb`.`vih_tarv`;
+DROP TABLE `sisvigdb`.`vih_form`;
